6bbf521aed4dc83a072eae545fe875ce585e4f93
Kilroy-creator <royhenry877@gmail.com>
2025-09-15 11:11:03 +0300

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Function expression
+const divide = function() {
+  return 2000 / 100;
+};
+
+// 2. Arrow function (one param, square it)
+const square = (x) => x * x;
+
+// 3. Arrow function (two params, add them)
+const add = (a, b) => a + b;
